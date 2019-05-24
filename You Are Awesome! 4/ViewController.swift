660d4c9948dc31faa3036c6d3feba199bf459945
Mary Otto <ottomc@bc.edu>
2019-05-24 17:41:05 -0500

    Initial Commit

---
+++ You Are Awesome! 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome! 4
+//
+//  Created by Mary Otto on 5/24/19.
+//  Copyright © 2019 Mary Otto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
