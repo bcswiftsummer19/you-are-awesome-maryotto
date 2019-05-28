4660f52f4c45246062cb2abfef49228770126c5e
Mary Otto <ottomc@bc.edu>
2019-05-28 10:53:50 -0500

    2.10 complete

--- You Are Awesome! 4/ViewController.swift
+++ You Are Awesome! 4/ViewController.swift
@@ -11,19 +11,38 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var inspirationLabel: UILabel!
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func messagePressed(_ sender: UIButton) {
-        if inspirationLabel.text == "you are awesome!" {
-            inspirationLabel.text = "you are great!"
-        } else if inspirationLabel.text == "you are great!" {
-            inspirationLabel.text = "you are amazing!" 
-            } else {
-                inspirationLabel.text = "you are awesome!"
-            }
+        
+        let messages = ["you are awesome!",
+                        "you are great!",
+                        "you are fantastic!",
+                        "when the genius bar needs help, they call you",
+                        "you brighten my day!",
+                        "you make me smile!"]
+        
+        inspirationLabel.text = messages[index]
+        if index == messages.count - 1 {
+            index = 0
+        } else {
+        index = index + 1
+        }
+        
+//        let message1 = "you are awesome!"
+//        let message2 = "you are great!"
+//        let message3 = "you are amazing!"
+//        if inspirationLabel.text == message1 {
+//            inspirationLabel.text = message2
+//        } else if inspirationLabel.text == message2 {
+//            inspirationLabel.text = message3
+//            } else {
+//                inspirationLabel.text = message1
+//            }
     }
     
 }
