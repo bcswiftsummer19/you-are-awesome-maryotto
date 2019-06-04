c0fd8821385a175876b739ed24e3f3cb1e5dffd7
Mary Otto <ottomc@bc.edu>
2019-06-04 12:38:49 -0500

    2.18 complete

--- You Are Awesome! 4/ViewController.swift
+++ You Are Awesome! 4/ViewController.swift
@@ -7,43 +7,89 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var inspirationLabel: UILabel!
+    @IBOutlet weak var inspirationImage: UIImageView!
     var index = -1
     var imageIndex = -1
+    var soundIndex = -1
     let numberOfImages = 42
-    @IBOutlet weak var inspirationImage: UIImageView!
+    let numberOfSounds = 6
+    var awesomePlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex: Int
+        repeat {
+            newIndex = Int.random(in: 0..<maxValue)
+        } while lastNumber == newIndex
+        return newIndex
+    }
+    
+    func playSound(soundName: String) {
+        //can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            } catch {
+                //if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound")
+            }
+            
+        } else {
+            //if reading in the NSDataAsset didn't work, tell the developer / report the error
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
     @IBAction func messagePressed(_ sender: UIButton) {
         
         let messages = ["you are awesome!",
                         "you are great!",
                         "you are fantastic!",
-                        "when the genius bar needs help, they call you",
+                        "every moment is a fresh beginning",
                         "you brighten my day!",
-                        "you make me smile!"]
-//        var newIndex = -1
-        var newIndex: Int // declares but does NOT initialize newIndex
-        repeat {
-            newIndex = Int.random(in: 0..<messages.count)
-        } while index == newIndex
-        
-        index = newIndex
+                        "you make me smile!",
+                        "whereever you go, go with all your heart",
+                        "it is never too late to be what you might have been",
+                        "tough times never last, but tough people do",
+                        "believe and act as if it were impossible to fail",
+                        "action is the foundational key to all success",
+                        "turn your wounds into wisdom",
+                        "don't regret the past, just learn from it",
+                        "just keep taking chances and having fun",
+                        "to improve is to change; to be perfect is to change often",
+                        "kindness is always fashionable, and always welcome",
+                        "be happy for this moment. this moment is your life",
+                        "if you are too busy to laugh, you are too busy",
+                        "it's a helluva start, being able to realize what makes you happy",
+                        "you can't cross the sea merely by standing and staring at the water",
+                        "aim for the moon. if you miss you may hit a star"]
+       
+        //show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
         inspirationLabel.text = messages[index]
-        
-        repeat {
-            newIndex = Int.random(in: 0..<numberOfImages)
-        } while imageIndex == newIndex
-        
-        imageIndex = newIndex
+       
+        //show and image
+        imageIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numberOfImages)
         inspirationImage.image = UIImage(named: "image\(imageIndex)")
         
+        //get a random number to use in our sound file
+        soundIndex = nonRepeatingRandom(lastNumber: soundIndex, maxValue: numberOfSounds)
+       
+        //play a sound
+        let soundName = "sound\(soundIndex)"
+        playSound(soundName: soundName)
+        }
     }
-}
+
+
 
